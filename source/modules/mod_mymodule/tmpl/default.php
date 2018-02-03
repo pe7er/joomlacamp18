7828979d4204acdddffb29bbb9f303e4168eca1c
Peter Martin <gitkraken@pe7er.com>
2018-02-03 14:18:01 +0100

    my initial module

---
+++ source/modules/mod_mymodule/tmpl/default.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package    mymodule
+ *
+ * @author     pe7er <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+defined('_JEXEC') or die;
+
+// Access to module parameters
+$domain = $params->get('domain', 'https://www.joomla.org');
+?>
+
+<a href="<?php echo $domain; ?>">
+	<?php echo 'mymodule'; ?>
+</a>
